3ff6179b9f620ddc4848e852f166a109b4076b1a
antixrist <ya.antixrist@gmail.com>
2015-12-07 12:36:44 +0400

    jsdoc definition

--- index.js
+++ index.js
@@ -8,35 +8,55 @@ var through = require('through2'),
 /**
  * @typedef {{}} GulpFont2Base64Opts
  * @property {string} template
- * @property {Function} map
+ * @property {GulpFont2Base64OptsMapper} map
  */
+
+/**
+ * @callback GulpFont2Base64OptsMapper
+ * @param {GulpFont2Base64FontData} data
+ * @returns {GulpFont2Base64FontData}
+ */
+
+/**
+ * @typedef {{}} GulpFont2Base64FontData
+ * @property {string} fontFamily Value for css-property 'font-family'. Default: file basename
+ * @property {string} fontStyle Value for css-property 'font-style'. Default 'normal',
+ * @property {string} fontWeight Value for css-property 'font-weight'. Default '400',
+ * @property {string} local It's a user's local font name. Value for css-property 'src: local(<...>)'. Default: file basename
+ * @property {string} format Similar to extname. If extname is 'ttf' then format will be equal to 'truetype'
+ * @property {string} mime Default: 'application/x-font-'+ format
+ * @property {string} base64 Base64 string of font content
+ */
+
 
 /**
  * @type {GulpFont2Base64Opts}
  */
 var defaults = {
+  map: function (data) {},
   template: [
     '@font-face {',
-    '  font-family: "<%= name %>";',
-    '  font-style: normal;',
-    '  font-weight: 400;',
-    '  src: local("<%= name %>"),',
+    '  <% if (fontFamily) { %>font-family: "<%= fontFamily %>";<% } %>',
+    '  <% if (fontStyle) { %>font-style: <%= fontStyle %>;<% } %>',
+    '  <% if (fontWeight) { %>font-weight: <%= fontWeight %>;<% } %>',
+    '  src: <% if (local) { %>local("<%= local %>"),<% } %>',
     '       url("data:<%= mime %>;base64,<%= base64 %>") format("<%= format %>");',
     '}'
-  ].join('\n'),
-  map: function () {}
+  ].join('\n')
 };
 
 /**
  * @param {GulpFont2Base64Opts} [opts]
  */
-module.exports = function(opts) {
+module.exports = function (opts) {
   opts = _.isPlainObject(opts) ? opts : {};
   opts = extend({}, defaults, opts);
 
   var tpl = _.template(opts.template);
+  /**
+   * @type {GulpFont2Base64OptsMapper}
+   */
   var mapper = _.isFunction(opts.map) ? opts.map : function () {};
-
 
   // create a stream through which each file will pass
   return through.obj(function(file, enc, callback) {
@@ -60,11 +80,14 @@ module.exports = function(opts) {
 
       var fileName = path.basename(file.path, extName);
 
+      /**
+       * @type {GulpFont2Base64FontData}
+       */
       var data = {
-        name: fileName,
-        localName: fileName,
+        fontFamily: fileName,
         fontStyle: 'normal',
         fontWeight: '400',
+        local: fileName,
         mime: 'application/x-font-'+ format,
         format: format,
         base64: base64
